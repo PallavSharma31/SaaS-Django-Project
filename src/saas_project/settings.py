21e008cc53893dccf5d8c8a8b52ee3710f2292ee
PallavSharma31 <pallavsharma2727@gmail.com>
2024-06-17 23:44:01 +0530

    update

--- src/saas_project/settings.py
+++ src/saas_project/settings.py
@@ -25,8 +25,15 @@ SECRET_KEY = 'django-insecure-bzun79*t=9@5$v^z%__p!uq4ana3uzbeeyz7h4(^@7z9y62w=$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
 
+if DEBUG:
+    ALLOWED_HOSTS+=[
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
--- src/saas_project/views.py
+++ src/saas_project/views.py
@@ -2,15 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from visits.models import PageVisit
 
-def home_page_view(requests,*args,**kwargs):
+
+def home_view(requests,*args,**kwargs):
+    return about_view(requests,*args,**kwargs)
+
+def about_view(requests,*args,**kwargs):
     qs=PageVisit.objects.all()
     page_qs=PageVisit.objects.filter(path=requests.path)
+
+    try:
+        percent=(page_qs.count()*100.0)/qs.count()
+    except:
+        percent=0    
 
     my_title="My Page"
     my_context= {
         "page_title":my_title,
         "page_visit_count":page_qs.count(),
-        "total_visit_count":qs.count()
+        "total_visit_count":qs.count(),
+        "percent":percent
     }
     path=requests.path
     print("path",path)
