846b205747caaa93efc5224ba540c88bd86ff19a
PallavSharma31 <pallavsharma2727@gmail.com>
2024-06-18 21:48:37 +0530

    neon production database added

--- src/saas_project/settings.py
+++ src/saas_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,14 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bzun79*t=9@5$v^z%__p!uq4ana3uzbeeyz7h4(^@7z9y62w=$'
+SECRET_KEY =config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
+
+DEBUG=config("DJANGO_DEBUG",cast=bool)
+
+print("Debug",DEBUG,type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
@@ -87,6 +93,20 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CONN_MAX_AGE=config("CONN_MAX_AGE",cast=int,default=30)
+DATABASE_URL=config("DATABASE_URL",cast=str,default=None)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES= {
+        "default":dj_database_url.config(
+            default=DATABASE_URL,
+            conn_health_checks=True,
+            conn_max_age=CONN_MAX_AGE,                                 
+        )
+    }
+
 
 
 # Password validation
